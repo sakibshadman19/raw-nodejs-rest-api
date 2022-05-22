acf06c432ddb078eb32b43fe3a93c5e2e9ee8cf5
Shadman <sakibshadman51@gmail.com>
2022-05-22 12:56:35 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Vanilla Node.js REST API  
+  
+
+<br/>
+
+I created Rest API without using any framework. Only Raw Nodejs was used in this project.
+<br/>
+
+
+### Author Links  
+
+👋 Hello,I am Shadman and I am a Full Stack Web developer  
+
+🚀 Follow me:  
+
+
+  - [Github](https://github.com/sakibshadman19)
+  - [LinkedIn](https://www.linkedin.com/in/shadman-sakib-95462923a/)
+  - [Facebook](https://www.facebook.com/shadman.sakibtanmoy)
+  
+
+
+<!-- all link is here -->
+
+
---
+++ controllers/productControllers.js
@@ -0,0 +1,128 @@
+// const { stringify } = require("nodemon/lib/utils"); 
+const Product = require("../models/productModel");
+const { getPostData } = require("../utils");
+
+async function getProducts(req,res){
+    try{
+        const products = await Product.findAll()
+        res.writeHead(200,{"Content-Type" : "application/json" })
+        res.end(JSON.stringify(products))
+
+    }
+    catch(error){
+        console.log(error);
+
+    }
+}
+
+async function getProduct(req,res,id){
+    try{
+        const product = await Product.findById(id)
+        if(!product){
+
+            res.writeHead(404,{"Content-Type" : "application/json" })
+            res.end(JSON.stringify({message : "not found"}))
+        }
+        else {
+            res.writeHead(200,{"Content-Type" : "application/json" })
+            res.end(JSON.stringify(product))
+        }
+
+    }
+    catch(error){
+        console.log(error);
+
+    }
+}
+
+async function createProduct(req,res){
+    try{
+                const body =  await getPostData(req)
+
+
+                const {title,description,price} = JSON.parse(body)
+  
+                
+                        const product ={
+                            title,
+                            description,
+                            price,
+                        }
+                        const newProduct = await Product.create(product)
+                        res.writeHead(201,{"Content-Type" : "application/json" })
+                        return res.end(JSON.stringify(newProduct))
+
+             
+
+
+    }
+    catch(error){
+        console.log(error);
+
+    }
+}
+async function updateProduct(req,res,id){
+    try{
+        const product = await Product.findById(id)
+
+        if(!product){
+            res.writeHead(404,{"Content-Type" : "application/json" })
+            res.end(JSON.stringify({message : "not found"}))
+        }else{
+
+            const body =  await getPostData(req)
+
+
+            const {title,description,price} = JSON.parse(body)
+
+            
+                    const productData ={
+                        title: title || product.title,
+                        description: description || product.description,
+                        price: price || product.price,
+                    }
+                    const updProduct = await Product.update(id,productData)
+                    res.writeHead(200,{"Content-Type" : "application/json" })
+                    return res.end(JSON.stringify(updProduct))
+
+         
+        }
+
+
+    }
+    catch(error){
+        console.log(error);
+
+    }
+}
+
+
+async function deleteProduct(req,res,id){
+    try{
+        const product = await Product.findById(id)
+        if(!product){
+
+            res.writeHead(404,{"Content-Type" : "application/json" })
+            res.end(JSON.stringify({message : "not found"}))
+        }
+        else {
+            await Product.remove(id)
+            res.writeHead(200,{"Content-Type" : "application/json" })
+            res.end(JSON.stringify({message: `product ${id} removed`}))
+        }
+
+    }
+    catch(error){
+        console.log(error);
+
+    }
+}
+
+
+module.exports ={
+    getProducts,
+    getProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
+}
